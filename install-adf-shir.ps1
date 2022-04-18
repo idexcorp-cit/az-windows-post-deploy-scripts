ede8f72b85425c206e4d6957b88b600f7984550d
Zachary Choate <zchoate@idexcorp.com>
2022-04-18 07:56:24 -0400

    add adf script

---
+++ install-adf-shir.ps1
@@ -0,0 +1,53 @@
+<#
+.DESCRIPTION
+Download SHIR, access key vault secret using VM system identity, and install SHIR.
+
+.NOTES
+Author: Zach Choate
+
+#>
+
+$appName = "Integration Runtime"
+$appUrl = 'http://go.microsoft.com/fwlink/?LinkID=839822'
+$installerType = "msi"
+$shirInstallScript = "https://raw.githubusercontent.com/Azure/Azure-DataFactory/main/SamplesV2/SelfHostedIntegrationRuntime/AutomationScripts/InstallGatewayOnLocalMachine.ps1"
+$shirScriptName = "InstallGatewayOnLocalMachine.ps1"
+$shirScriptPath = $env:TEMP + "\" + $shirScriptName
+
+$timestamp = Get-Date -f yyyyMMddHHmmss
+
+$installerFileName = $appName + "." + $installerType
+$installerPath = $env:TEMP + "\" + $installerFileName
+$transcriptPath = "$env:TEMP\$timestamp-install-$appName.log"
+
+Start-Transcript -Path $transcriptPath -Append
+
+# Get the resource group of the VM, the key vault should be located in the same resource group
+$resourceGroup = (Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -NoProxy -Uri "http://169.254.169.254/metadata/instance?api-version=2021-02-01").compute.resourceGroupName
+
+# Connect to Az to get KV secret
+Connect-AzAccount -Identity
+
+# Get vault name
+$vaultName = (Get-AzKeyVault -ResourceGroupName $resourceGroup).vaultName
+
+# Get Key Vault Secret
+$shirKey = Get-AzKeyVaultSecret -VaultName $vaultName -AsPlainText -Name "shir-auth-key"
+
+# Download the installer
+Invoke-RestMethod -Method GET -Uri $appUrl -OutFile "$installerPath"
+
+# Download SHIR installer script
+Invoke-RestMethod -Method GET -Uri $shirInstallScript -OutFile $shirScriptPath
+
+# Make sure the installer downloaded successfully
+if((Test-Path $installerPath) -and (Test-Path $shirScriptPath)) {
+    Try {
+        Start-Process powershell.exe -ArgumentList "-ExecutionPolicy Bypass -File $shirScriptPath -path $installerPath -authKey $shirKey" -Wait
+    } Catch {
+        Write-Error $_
+    }
+} else {
+    Write-Output "The installer doesn't look like it downloaded successfully."
+    Exit 1
+}
